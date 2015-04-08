dbfae878750ee04bbae06eb1f2cfc63af2b73da4
Yanghg <yanghong2014@u.northwestern.edu>
2015-04-08 18:41:58 -0500

    remove hello

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,9 +1,7 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+
 
 Parse.Cloud.define("getDepart", function(request, response) {
   var query = new Parse.Query("OrderRec");
