d1816fba42a9be28c9994494a4b011704645dc0a
yanghuirong <rongyanghui@gmail.com>
2015-04-08 18:29:06 -0500

    add join button

--- app/example/scripts/myscript.js
+++ app/example/scripts/myscript.js
@@ -1,3 +1,9 @@
 function testFunction() {
-   document.getElementById("my_button").innerHTML="Leave Taxi";         // The function returns the product of p1 and p2
+    document.getElementById("my_button").innerHTML="Leave Taxi";         // The function returns the product of p1 and p2
+}
+function join()
+{
+    var element = document.getElementById("mail");
+    element.innerHTML = element.innerHTML+'Sheldon@gmail.com';
+    document.getElementById("join_button").disabled = true;
 }
