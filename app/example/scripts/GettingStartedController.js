2bbd08048dabd6caac3ce0b1bab8c64e05324008
zhangjuli <julizhang2014@u.northwestern.edu>
2015-04-08 22:16:57 -0500

    final demo

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -3,4 +3,10 @@ angular
   .controller('GettingStartedController', function($rootScope, $scope, supersonic) {
     //$scope.navbarTitle = "FareShare";
     $rootScope.cow = "Northwestern";
+      $scope.seat=3;
+      $scope.join=function()
+      {
+          if($scope.seat>0)
+            $scope.seat=$scope.seat-1;
+      }
   });
