378f7d69fd05f40084dd415f6f85a2440e91d74e
brandonstaunton <brandonstaunton2015@u.northwestern.edu>
2015-04-09 20:01:20 -0500

    Added backend integration for taxi creation

--- app/example/scripts/FormPartyController.js
+++ app/example/scripts/FormPartyController.js
@@ -1,8 +1,21 @@
 angular
   .module('example')
-  .controller('FormPartyController', function($rootScope, $scope, supersonic) {
-    //$scope.navbarTitle = "Form Party";
-    $scope.myfunction = function() {
-    	alert("Form Submitted! New Taxi Created.");
+  .controller("FormPartyController", function ($scope, Taxidata, supersonic) {
+    $scope.taxidata = {};
+
+    $scope.submitForm = function () {
+      $scope.showSpinner = true;
+      newtaxidata = new Taxidata($scope.taxidata);
+      newtaxidata.save().then( function () {
+  			
+  			alert("Form Submitted! New Taxi Created.");
+  			supersonic.ui.tabs.select(0);
+
+      	});
+    };
+
+    $scope.cancel = function () {
+      supersonic.ui.modal.hide();
     }
+
   });
