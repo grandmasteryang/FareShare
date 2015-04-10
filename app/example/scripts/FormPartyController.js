ab94051865df4b05c5c7abd5beb400929a914b7e
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-04-10 15:53:57 -0500

    added google geolocation

--- app/example/scripts/FormPartyController.js
+++ app/example/scripts/FormPartyController.js
@@ -3,15 +3,32 @@ angular
   .controller("FormPartyController", function ($scope, Taxidata, supersonic) {
     $scope.taxidata = {};
 
+    //google geolocation for departure
+    $scope.departClick = function() {
+      $scope.departureInput = document.getElementById("departureLocation");
+      $scope.autocompleteDep = new google.maps.places.Autocomplete($scope.departureInput); 
+      google.maps.event.addListener(autocompleteDep, 'place_changed', function() {
+            $scope.departureInput=autocompleteDep.getPlace().geometry.location;
+      });
+    }
+
+    //google geolocation for destination
+    $scope.destClick = function() {
+      $scope.destInput = document.getElementById("destination");
+      $scope.autocompleteDest = new google.maps.places.Autocomplete($scope.destInput); 
+      google.maps.event.addListener(autocompleteDest, 'place_changed', function() {
+            $scope.destInput=autocompleteDest.getPlace().geometry.location;
+      });
+    }
+
     $scope.submitForm = function () {
+      //store info in parse
+      alert($scope.departureInput);
       $scope.showSpinner = true;
       newtaxidata = new Taxidata($scope.taxidata);
       newtaxidata.save().then( function () {
-  			
-  			alert("Form Submitted! New Taxi Created.");
-  			supersonic.ui.tabs.select(0);
-
-      	});
+        supersonic.ui.tabs.select(0);
+      });
     };
 
     $scope.cancel = function () {
