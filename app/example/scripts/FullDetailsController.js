ca1de3d4f96d4aab3388c81892a05850ce7d8c50
Yanghg <yanghong2014@u.northwestern.edu>
2015-04-13 10:30:31 -0500

    get join and leave taxi in full-detail fixed

--- app/example/scripts/FullDetailsController.js
+++ app/example/scripts/FullDetailsController.js
@@ -15,8 +15,21 @@ angular
     }
 
     $scope.joinButton = "Join Taxi";
-    $scope.clickJoin = function() {
-      $scope.joinButton = "Leave Taxi";
+    $scope.clickJoin = function(taxidata) {
+      //$scope.joinButton = "Leave Taxi";  
+      if (taxidata['remainingSeats'] == 0){
+        alert("full!");
+      }    
+      else if ($scope.joinButton == "Join Taxi") {
+        $scope.joinButton = "Leave Taxi";
+          taxidata['remainingSeats']--;
+          taxidata.save();
+      }
+      else if ($scope.joinButton == "Leave Taxi") {
+        $scope.joinButton = "Join Taxi";
+          taxidata['remainingSeats']++;
+          taxidata.save();
+      }
       //$location.path("getting-started");
     };
   
