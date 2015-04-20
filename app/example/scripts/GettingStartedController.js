15cc397a1affa2f8ae860a0d38c9dd378a1b444b
zhangjuli <julizhang2014@u.northwestern.edu>
2015-04-19 22:57:05 -0500

    Add the filter of time

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -14,14 +14,16 @@ angular
       location.reload();
     };
     $scope.myfilter = function(element){
-      var newString = String($scope.query.departDate.getMonth() + 1) + "/"
+      var DateString = String($scope.query.departDate.getMonth() + 1) + "/"
           + String($scope.query.departDate.getDate()) + "/" 
           + String($scope.query.departDate.getFullYear());
+      var TimeString = String($scope.query.departTime.getHours()) + ":"
+          + String($scope.query.departTime.getMinutes());
       return(
-        ((element['deptAddr'].indexOf($scope.query.deptAddr))>=0)&&(element['destAddr'].indexOf($scope.query.destAddr)>=0)&&(
-          element['departDate'] == newString)
+        ((element['deptAddr'].indexOf($scope.query.deptAddr))>=0)&&(element['destAddr'].indexOf($scope.query.destAddr)>=0)
+        &&(element['departDate'] == DateString)&&(element['departTime'] == TimeString)
         );
-    }
+    } 
     //$scope.query = function(){
     
     //$scope.query.date=String($scope.query.date.getMonth() + 1) + "/"
