f44c6a0e66f9a7460fca1ff2f025bb30034adb0e
yanghuirong <rongyanghui@gmail.com>
2015-04-20 00:24:01 -0500

    for pull

--- app/example/scripts/loginController.js
+++ app/example/scripts/loginController.js
@@ -1,6 +1,6 @@
 angular
   .module('example')
-  .controller("LoginController", function ($scope, Taxidata, userTable, supersonic) {
+  .controller("LoginController", function ($scope, Taxidata, supersonic) {
  
 
   });
