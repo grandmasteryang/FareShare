0f1990039dabbab195b10df3e24ac9e38a651840
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-04-20 21:06:40 -0500

    filter distances

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -2,17 +2,12 @@ angular
   .module('example')
   .controller("GettingStartedController", function ($scope, Taxidata, supersonic) {
     $scope.taxidatas = null;
-   
 
     Taxidata.all().whenChanged( function (taxidatas) {
         $scope.$apply( function () {
           $scope.taxidatas = taxidatas;  
     	});	
     });
-
-    $scope.refreshTaxis = function() {
-      location.reload();
-    };
 
     //google geolocation for departure
     $scope.departClick = function() {
@@ -21,7 +16,7 @@ angular
       google.maps.event.addListener(autocompleteDep, 'place_changed', function() {
             $scope.departureInput=autocompleteDep.getPlace().geometry.location;
       });
-    }
+    };
 
     //google geolocation for destination
     $scope.destClick = function() {
@@ -30,7 +25,7 @@ angular
       google.maps.event.addListener(autocompleteDest, 'place_changed', function() {
             $scope.destInput=autocompleteDest.getPlace().geometry.location;
       });
-    }
+    };
 
     $scope.myfilter = function(element){
       var DateString = String($scope.query.departDate.getMonth() + 1) + "/"
@@ -38,11 +33,26 @@ angular
           + String($scope.query.departDate.getFullYear());
       var TimeString = String($scope.query.departTime.getHours()) + ":"
           + String($scope.query.departTime.getMinutes());
+
+      var filterDept = new google.maps.LatLng($scope.departureInput.lat(), $scope.departureInput.lng());
+      var filterDest = new google.maps.LatLng($scope.destInput.lat(), $scope.destInput.lng());
+      var dbDept = new google.maps.LatLng(element['deptObj'].D, element['deptObj'].k);
+      var dbDest = new google.maps.LatLng(element['destObj'].D, element['destObj'].k);
+      $scope.distanceDept = google.maps.geometry.spherical.computeDistanceBetween(filterDept, dbDept);
+      $scope.distanceDest = google.maps.geometry.spherical.computeDistanceBetween(filterDest, dbDest);
+
       return(
         ((element['deptAddr'].indexOf($scope.query.deptAddr))>=0)&&(element['destAddr'].indexOf($scope.query.destAddr)>=0)
         &&(element['departDate'] == DateString)&&(element['departTime'] == TimeString)
         );
-    } 
+    }; 
+
+    $scope.refreshTaxis = function() {
+      //location.reload();
+      alert($scope.distanceDept);
+      //alert("HI");
+    };
+
     //$scope.query = function(){
     
     //$scope.query.date=String($scope.query.date.getMonth() + 1) + "/"
@@ -54,14 +64,8 @@ angular
 //    $scope.filterByDate = function(taxidata){
 //        return $scope.queryDate[taxidata.departDate];
 //    }
-    // $scope.updateSeats = function(taxidata) {
-    //   if (taxidata['remainingSeats'] == 0) {
-    //     alert("No available seats!")
-    //   }
-    // 	else {
-    // 		taxidata['remainingSeats']--;
-    // 		taxidata.save();
-    // 	}
-    // };
+    $scope.updateSeats = function(taxidata) {
+      alert(taxidata['destObj'].k);
+    };
 
   });
