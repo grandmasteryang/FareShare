218ecb1a0d5b035874ab245c2d35eb692423f5f1
Yanghg <yanghong2014@u.northwestern.edu>
2015-04-20 19:03:48 -0500

    finished login and signup

--- app/example/scripts/LoginController.js
+++ app/example/scripts/LoginController.js
@@ -5,34 +5,69 @@ angular
     $scope.hello = "Taxi";
     $scope.userdata = {};
     $scope.userdatas = null;
-
+    //$scope.users = null;
     $scope.loginButton = "Login";
-    $scope.clickJoin = function(taxidata) {
-      $scope.username = document.getElementById('username').value;
-      $scope.password = document.getElementById('password').value;
-      $scope.loginButton = "wahaha";
-      //var query = { "userName": "pudding" };
-    };
-    /*
-    Usertable.find('JAirU9e11W').then( function (userdata) {
-      $scope.$apply( function () {
-        $scope.userdata = userdata;
-      });
-    });*/
-    var query = { "userName": "pudding" };
-    Usertable.findAll({query: JSON.stringify(query)}).then( function (userdatas) {
-      
-        $scope.userdatas = userdatas;
-      
+
+    Usertable.findAll().then( function (userdatas) {
+         $scope.userdatas = userdatas;         
     });
-  
+
+    $scope.clickLogin = function() {
+      var flag = false;
+      $scope.username = document.getElementById('login-username').value;
+      $scope.password = document.getElementById('login-password').value;
+
+      for (i = 0; i < $scope.userdatas.length; i++){
+        if ($scope.userdatas[i].userName == $scope.username && $scope.userdatas[i].userPassword == $scope.password){           
+          flag = true;
+          supersonic.ui.tabs.select(0);
+          //window.open("getting-started.html")
+          break;
+        }          
+      }
+
+      if (flag == false){
+        alert("Invalid username or password!");
+      }
+    };
     
+    $scope.signupButton = "Sign Up";
+    $scope.clickSignup = function() {
+      if ($scope.signupButton == "Sign Up"){
+        var flag = false;
+        $scope.userdata['userName'] = document.getElementById('signup-username').value;
+        $scope.userdata['userPassword'] = document.getElementById('signup-password').value;
+        for (i = 0; i < $scope.userdatas.length; i++){
+          if ($scope.userdata['userName']  ==  $scope.userdatas[i].userName ){           
+            flag = true;
+  
+            break;
+          }          
+        }
+        if (flag == false){
+          newuserdata = new Usertable($scope.userdata);
+          newuserdata.save();
+          $scope.signupButton = "Login";
+          alert("Successfully sign up!");
+        }
+        else {
+          alert("Same username existed!! Input a new one:)");
+        }
+      }
+      else if ($scope.signupButton == "Login"){
+        supersonic.ui.tabs.select(0);
+      }
+    };
 
-    /*Usertablge.all().whenChanged( function (userdatas) {
-        $scope.$apply( function () {
-          $scope.userdatas = userdatas;  
-        }); 
-    });*/
-
+    /*
+    var usert = supersonic.data.model('userTable');
+    var query = {"lastName": "Diaz"};
+    usert.findAll({query: JSON.stringify(query)}).then(function(users){
+      $scope.$apply( function () {
+        $scope.users = users;
+        supersonic.logger.log(users);
+      });
+    });
+    */
 
   });
