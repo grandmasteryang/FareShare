666740c3bc35fbb219fd66df687acb1a0f6f8ae9
Yanghg <yanghong2014@u.northwestern.edu>
2015-04-20 19:31:13 -0500

    add some changes to signup

--- app/example/scripts/LoginController.js
+++ app/example/scripts/LoginController.js
@@ -35,23 +35,29 @@ angular
     $scope.clickSignup = function() {
       if ($scope.signupButton == "Sign Up"){
         var flag = false;
+        $scope.userdata['firstName'] = document.getElementById('signup-firstname').value;
+        $scope.userdata['lastName'] = document.getElementById('signup-lastname').value;
         $scope.userdata['userName'] = document.getElementById('signup-username').value;
         $scope.userdata['userPassword'] = document.getElementById('signup-password').value;
+        $scope.userdata['phoneNumber'] = document.getElementById('signup-phonenumber').value;
         for (i = 0; i < $scope.userdatas.length; i++){
           if ($scope.userdata['userName']  ==  $scope.userdatas[i].userName ){           
             flag = true;
-  
             break;
           }          
         }
-        if (flag == false){
-          newuserdata = new Usertable($scope.userdata);
-          newuserdata.save();
-          $scope.signupButton = "Login";
-          alert("Successfully sign up!");
-        }
-        else {
-          alert("Same username existed!! Input a new one:)");
+        if ($scope.userdata['firstName'] == "" || $scope.userdata['lastName'] == "" || $scope.userdata['userName'] == "" || $scope.userdata['userPassword'] == "" || $scope.userdata['phoneNumber'] == ""){
+          alert("Please fill in all blank areas!");
+        } else {
+          if (flag == false){
+            newuserdata = new Usertable($scope.userdata);
+            newuserdata.save();
+            $scope.signupButton = "Login";
+            alert("Successfully sign up!");
+          }
+          else {
+            alert("Same username existed!! Input a new one:)");
+          }
         }
       }
       else if ($scope.signupButton == "Login"){
