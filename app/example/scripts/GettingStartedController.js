cd9bb7952303603ccbf75e67f18218ddd23cace9
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-04-22 23:22:06 -0500

    filter working almost completely, minor bug with dest and dept

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -2,6 +2,8 @@ angular
   .module('example')
   .controller("GettingStartedController", function ($scope, Taxidata, supersonic) {
     $scope.taxidatas = null;
+    var filterDept;
+    var filterDest;
 
     Taxidata.all().whenChanged( function (taxidatas) {
         $scope.$apply( function () {
@@ -16,8 +18,6 @@ angular
       google.maps.event.addListener(autocompleteDep, 'place_changed', function() {
             $scope.departureInput=autocompleteDep.getPlace().geometry.location;
             filterDept = new google.maps.LatLng($scope.departureInput.lat(), $scope.departureInput.lng());
-            //computeDistance returns distance in meters, divide by 1609 to change to miles
-            $scope.distanceDept = google.maps.geometry.spherical.computeDistanceBetween(filterDept, dbDept)/1609.34;
       });
     };
 
@@ -28,20 +28,17 @@ angular
       google.maps.event.addListener(autocompleteDest, 'place_changed', function() {
             $scope.destInput=autocompleteDest.getPlace().geometry.location;
             filterDest = new google.maps.LatLng($scope.destInput.lat(), $scope.destInput.lng());
-            //computeDistance returns distance in meters, divide by 1609 to change to miles
-            $scope.distanceDest = google.maps.geometry.spherical.computeDistanceBetween(filterDest, dbDest)/1609.34;
       });
     };
 
     $scope.myfilter = function(element){
-
       var DateString = "";
       var timeBool = true;
       var timeInt1 = "";
       var parsedint1 = 0;
       var parsedint2 = 0;
-     
-
+      var withinDept = true;
+      var withinDest = true;
       
       if ($scope.departDate)
       {
@@ -49,52 +46,39 @@ angular
           + String($scope.departDate.getDate()) + "/" 
           + String($scope.departDate.getFullYear());
       }
-       
       if ($scope.departTime)
       {
-        // $scope.departTime is an object
-        // element['departTime'] is a string
-
-    
         timeINT1 = element['departTime'].split(":");
-
-
         parsedint1 = (60* parseInt(timeINT1[0])) + parseInt(timeINT1[1]);
         parsedint2 = 60 * $scope.departTime.getHours() + $scope.departTime.getMinutes();
-      
-
         if (Math.abs(parsedint1 - parsedint2) > 60.0)
         {
           timeBool = false;
         }
       }
 
-
-      
-      
       var dateBool = (element['departDate'] == DateString);
-      //take out
       dateBool = dateBool || (!$scope.departDate);
-      
 
-     
-      var withinDept = true;
-      var withinDest = true;
-
-      
       dbDept = new google.maps.LatLng(element['deptObj'].k, element['deptObj'].D);
       dbDest = new google.maps.LatLng(element['destObj'].k, element['destObj'].D);
-
-      //if departure location and filter is within
-      if ($scope.distanceDept > 5) {
-        withinDept = false;
+      if (filterDept!=undefined) {
+        //computeDistance returns distance in meters, divide by 1609 to change to miles
+        $scope.distanceDept = google.maps.geometry.spherical.computeDistanceBetween(filterDept, dbDept)/1609.34;
+        //if departure location and filter is within x miles
+        if ($scope.distanceDept > 5) {
+          withinDept = false;
+        }
       }
-      //if destination location and filter is within
-      if ($scope.distanceDest > 5) {
-        withinDest = false;
+      if (filterDest!=undefined) {
+        //computeDistance returns distance in meters, divide by 1609 to change to miles
+        $scope.distanceDest = google.maps.geometry.spherical.computeDistanceBetween(filterDest, dbDest)/1609.34;
+        //if destination location and filter is within x miles
+        if ($scope.distanceDest > 5) {
+          withinDest = false;
+        }
       }
       
-
       return (timeBool && dateBool && withinDept && withinDest);
     }; 
 
