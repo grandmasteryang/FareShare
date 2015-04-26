cd2179d7abd95912e9da3e457841a755e68c1e88
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-04-26 18:46:21 -0500

    get row with correct objectid

--- app/example/scripts/MyTaxiController.js
+++ app/example/scripts/MyTaxiController.js
@@ -1,6 +1,28 @@
 angular
   .module('example')
   .controller("MyTaxiController", function ($scope, Taxidata, Usertable, supersonic) {
-  	          $scope.objectId = localStorage.objectId;
+  	$scope.userdatas = null;
+  	$scope.taxidatas = null;
+  	$scope.userRow = null;
+  	$scope.objectId = localStorage.objectId;
 
+  	Usertable.all().whenChanged( function (userdatas) {
+         $scope.userdatas = userdatas;         
+    });
+
+    Taxidata.all().whenChanged( function (taxidatas) {
+        $scope.$apply( function () {
+          $scope.taxidatas = taxidatas;  
+    	});	
+    });
+
+    $scope.test = function() {
+	    for (var i = 0; i < $scope.userdatas.length; i++) {
+	    	if ($scope.userdatas[i].id === $scope.objectId) {
+	    		$scope.userRow = $scope.userdatas[i];
+	    		break;
+	    	}
+	    }
+	    alert($scope.userRow.id);
+    }
   });
