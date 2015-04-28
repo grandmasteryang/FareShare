da871a4d24ad27104a367690805f02a82f598e17
brandonstaunton <brandonstaunton2015@u.northwestern.edu>
2015-04-27 19:45:46 -0500

    Changed leaving and joining

--- app/example/scripts/FullDetailsController.js
+++ app/example/scripts/FullDetailsController.js
@@ -7,8 +7,7 @@ angular
     $scope.userId = localStorage.objectId;
     $scope.userdata = null;
     $scope.joinedTaxiIds = [];
-
-
+    $scope.joinedBool = false;
 
     Usertable.find($scope.userId).then( function (user) {
       $scope.userdata = user;
@@ -20,39 +19,68 @@ angular
         $scope.$apply( function () {
           $scope.taxidata = taxidata;
           $scope.showSpinner = false;
+          $scope.joinedBool = ($scope.joinedTaxiIds.indexOf($scope.taxidata['id']) >= 0);
+
         });
       });
     }
 
+    
+
     document.getElementById("user-info").innerHTML = "User: " + localStorage.username2 ;
     $scope.joinButton = "Join Taxi";
 
-    $scope.clickJoin = function(taxidata) {
-
-      if ($scope.joinButton == "Join Taxi") {
-
+    $scope.clickJoin = function() {
         // update the information for this taxi
         // this includes adding this user and updating the number of remaining seats
         //$scope.joinButton = "Leave Taxi";
-        taxidata['remainingSeats']--;
-        taxidata['passengerList'] = Array($scope.userdata['firstName']).concat(taxidata['passengerList']);
-        taxidata.save();
+        $scope.taxidata['remainingSeats']--;
+        $scope.taxidata['passengerList'] = Array($scope.userdata['firstName']).concat($scope.taxidata['passengerList']);
+        $scope.taxidata.save();
 
         // update the information for this user
-        $scope.joinedTaxiIds.push(taxidata['id']);
+        $scope.joinedTaxiIds.push($scope.taxidata['id']);
         $scope.userdata['joinedTaxis'] = $scope.joinedTaxiIds;
         $scope.userdata.save();
 
         // navigate to a new page
-        
         supersonic.ui.tabs.select(0);
-      }
-      else if ($scope.joinButton == "Leave Taxi") {
-        $scope.joinButton = "Join Taxi";
-          taxidata['remainingSeats']++;
-          taxidata.save();
-          alert("You left the Taxi.");
-      }
+    
+    };
+
+    $scope.clickLeave = function() {
+        
+        // update the taxi to reflect that we left
+        $scope.taxidata['remainingSeats']++;
+        var tmpArr = $scope.taxidata['passengerList'];
+        var index = tmpArr.indexOf($scope.userdata['firstName']);
+        alert(index);
+        if(tmpArr.length == 1){
+          tmpArr = [];
+        } else{
+          tmpArr.splice(index, 1);
+        }    
+        
+        $scope.taxidata['passengerList'] = tmpArr;
+
+        //$scope.taxidata.save();
+
+        // update the information for this user to reflect that he left
+        tmpArr = $scope.userdata['joinedTaxis'];
+        index = tmpArr.indexOf($scope.taxidata['id']);
+        alert(index);
+        if(tmpArr.length == 1){
+          tmpArr = [];
+        } else{
+          tmpArr.splice(index, 1);
+        }
+        $scope.userdata['joinedTaxis'] = tmpArr;
+        //$scope.userdata.save();
+
+
+        // navigate to a new page
+        supersonic.ui.tabs.select(0);
+    
     };
   
 
