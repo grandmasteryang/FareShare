84e8526a98e43968e4c207064f0b04fe4e202f0a
brandonstaunton <brandonstaunton2015@u.northwestern.edu>
2015-04-29 20:14:53 -0500

    Added time parsing to my-taxi

--- app/example/scripts/MyTaxiController.js
+++ app/example/scripts/MyTaxiController.js
@@ -44,5 +44,35 @@ angular
             });  
     });
 
+  $scope.myfilter = function(element){
+      if (element['departTime'].charAt(element['departTime'].length-2) == ":") {
+        element['departTime'] += "0";
+      }
+
+      if (element['departTime'].length == 4) {
+        if (element['departTime'].charAt(0) == 0) {
+          element['departTime'] = "12" + ":" + element['departTime'].charAt(2) + element['departTime'].charAt(3);
+        } 
+        element['departTime'] += " AM";
+      }
+
+      else if (element['departTime'].length == 5) {
+        var hour = (Number(element['departTime'].charAt(0))*10) + (Number(element['departTime'].charAt(1)));
+        if (hour > 12) {
+          hour -= 12;
+          element['departTime'] = hour.toString() + ":" + element['departTime'].charAt(3) + element['departTime'].charAt(4)
+          + " PM";
+        }
+        else if (hour == 10 || hour == 11) {
+          element['departTime'] += " AM";
+        }
+        else if (hour == 12) {
+          element['departTime'] += " PM";
+        }
+      }
+
+      return true;
+    };
+
 
   });
