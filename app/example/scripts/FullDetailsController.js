b6cfb6ad4212b56fb8e7f1b14d5ca161a7d4450c
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-05-19 11:35:04 -0500

    added google map api key

--- app/example/scripts/FullDetailsController.js
+++ app/example/scripts/FullDetailsController.js
@@ -8,6 +8,15 @@ angular
     $scope.userdata = null;
     $scope.joinedTaxiIds = [];
 
+    function initialize() {
+      var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 
     Taxidata.all().whenChanged( function (taxidatas) {
         $scope.$apply( function () {
