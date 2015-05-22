aa10c693bee16d0261c8110f5390ad68c37449fa
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-05-22 15:55:02 -0500

    fixed joinedbool bug omfg

--- app/example/scripts/FullDetailsController.js
+++ app/example/scripts/FullDetailsController.js
@@ -16,10 +16,92 @@ angular
     });
 
     Usertable.all().whenChanged( function (userdatas) {
-      Usertable.find($scope.userId).then( function (user) {
-        $scope.userdata = user;
-        $scope.joinedTaxiIds = user['joinedTaxis'];
-        $scope.joinedBool = ($scope.joinedTaxiIds.indexOf($scope.taxidata['id']) >= 0);
+      Taxidata.find($scope.dataId).then( function (taxidata) {
+        Usertable.find($scope.userId).then( function (user) {
+          $scope.$apply( function () {
+            $scope.userdata = user;
+            $scope.taxidata = taxidata;
+            $scope.joinedTaxiIds = user['joinedTaxis'];
+            $scope.joinedBool = ($scope.joinedTaxiIds.indexOf($scope.taxidata['id']) >= 0);
+
+            var mapOptions = {
+              center: { lat: 42.055984, lng: -87.675171},
+              zoom: 12
+            };
+            var map = new google.maps.Map(document.getElementById('map-canvas'),
+                   mapOptions);
+            var deptMarker = new google.maps.Marker({
+              position: { lat: $scope.taxidata['deptObj'].A, lng: $scope.taxidata['deptObj'].F},
+              map: map
+            });
+            var destMarker = new google.maps.Marker({
+              position: { lat: $scope.taxidata['destObj'].A, lng: $scope.taxidata['destObj'].F},
+              map: map
+            });
+
+            var markerList = new Array(deptMarker.position, destMarker.position);
+            var bounds = new google.maps.LatLngBounds();
+
+            for (var i=0, LtLgLen = markerList.length; i<LtLgLen; i++) {
+                bounds.extend(markerList[i]);
+            }
+
+            map.fitBounds(bounds);
+            google.maps.event.trigger(map,'resize');
+
+            var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
+            var directionsService = new google.maps.DirectionsService();
+
+            function calcRoute() {
+              var request = {
+                  origin:deptMarker.position,
+                  destination:destMarker.position,
+                  travelMode: google.maps.TravelMode.DRIVING
+              };
+              directionsService.route(request, function(response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                  directionsDisplay.setDirections(response);
+                }
+              });
+            }
+
+            calcRoute();
+            directionsDisplay.setMap(map);
+
+            //Time parsing
+            $scope.time = taxidata['departTime'];
+            if ($scope.time.charAt($scope.time.length-2) ==":" && $scope.time.charAt($scope.time.length-1) != 0) {
+              $scope.time = $scope.time.substring(0, $scope.time.length-1) + "0" + 
+              $scope.time.charAt($scope.time.length-1);
+            }
+
+            if ($scope.time.charAt($scope.time.length-2) == ":") {
+              $scope.time += "0";
+            }
+
+            if ($scope.time.length == 4) {
+              if ($scope.time.charAt(0) == 0) {
+                $scope.time = "12" + ":" + $scope.time.charAt(2) + $scope.time.charAt(3);
+              } 
+              $scope.time += "AM";
+            }
+
+            else if ($scope.time.length == 5) {
+              var hour = (Number($scope.time.charAt(0))*10) + (Number($scope.time.charAt(1)));
+              if (hour > 12) {
+                hour -= 12;
+                $scope.time = hour.toString() + ":" + $scope.time.charAt(3) + $scope.time.charAt(4)
+                + "PM";
+              }
+              else if (hour == 10 || hour == 11) {
+                $scope.time += "AM";
+              }
+              else if (hour == 12) {
+                $scope.time += "PM";
+              }
+            }
+          });
+        });
       });
     });
 
